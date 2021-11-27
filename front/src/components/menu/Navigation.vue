340857dd2b994a089a700f58c5e18c0c415fbeac
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-11-27 16:26:30 +0700

    #11 create menu bar

---
+++ front/src/components/menu/Navigation.vue
@@ -0,0 +1,72 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <h3>Username</h3>
+        </li>
+        <li>
+          <a href="">My events</a>
+        </li>
+        <li>
+          <a href="">Find events</a>
+        </li>
+        <li>
+          <a href="">Categories</a>
+        </li>
+        <li>
+          <span class="glyphicon">&#xe163;</span>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    margin: auto;
+    margin-top: 20px;
+    width: 90%;
+    height: 7rem;
+    border-radius: 12px;
+    background-color: #f4f4f5;
+  }
+
+  nav {
+    height: 100%;
+  }
+
+  ul {
+    list-style: none;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  li {
+    margin: 0 0.5rem;
+  }
+
+  a {
+    text-decoration: none;
+    background: transparent;
+    cursor: pointer;
+    color: rgb(12, 12, 12);
+    padding: 1.7rem 2rem;
+    display: inline-block;
+    font-family: Sans-serif;
+    font-size: 2.5rem;
+  }
+
+  a:hover,
+  a:active {
+    color: #0c0c0c;
+    border-color: #0c0c0c;
+    background-color: yellow;
+  }
+
+  span {
+    font-size: 3.5rem;
+  }
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+// import { createRouter, createWebHistory} from "vue-router";
+// import SignIn from "./components/authentication/SignIn.vue";
+// import SignOut from "./components/authentication/SignOut.vue";
+// import SignUp from "./components/authentication/SignUp.vue";
+// import Navigation from "./components/menu/Navigation.vue";
+
+const app = createApp(App);
+// app.component("sign-in", SignIn);
+// app.component("sign-out", SignOut);
+// app.component("sign-up", SignUp);
+// app.component("nav-bar", Navigation);
+
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes: [
+//         { path: '/', component: Navigation},
+//         // { path: '/signin', component: SignIn},
+//         // { path: '/signup', component: SignUp },
+//     ],
+// });
+// app.use(router)
+app.mount("#app");
