2401d02a08403202286c2d2eae1954178c4ecfb2
SOM_CHHORN <som.chhorn@student.passerellesnumeriques.org>
2021-11-28 12:58:52 +0700

    #6 get user in api

--- back/routes/api.php
+++ back/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 // PUBLIC ROUTE
+
 Route::get('/user', [UserController::class, 'index']);
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
