e111636d0c86b44243e14d5ca567e04187e19617
Lyden Chai <lyden.chai@PNCLB12-23.pnc.lan>
2021-11-28 17:00:12 +0700

    #7 fix sign up component

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,27 +5,7 @@
 </template>
 
 <script>
-    import SignUp from './components/authentication/SignUp.vue';
-    export default {
-        components: { SignUp },
-        data() {
-            return {
-                users: []
-            };
-        },
-        methods: {
-            signUp(first_name, last_name, phone_number, email, password, confirm_password){
-                let newUser = {
-                    first_name: first_name,
-                    last_name: last_name,
-                    phone_number: phone_number,
-                    email: email,
-                    password: password,
-                    confirm_password: confirm_password,
-                }
-            },
-        },
-    }
+    
 </script>
 
 <style lang="scss" scoped>
