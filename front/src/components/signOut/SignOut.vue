7ebd158acad30a8438ccf3d4bf564b82d88a2ffd
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-11-28 18:29:55 +0700

    #11 create menu bar is done

--- front/src/components/signOut/SignOut.vue
+++ front/src/components/signOut/SignOut.vue
@@ -1,6 +1,6 @@
 <template> 
     <div> 
- 
+        <h1>Sign Out</h1>
     </div> 
 </template> 
  
--- front/src/main.js
+++ front/src/main.js
@@ -4,7 +4,7 @@ import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import Navigation from "./components/menu/Navigation.vue";
 import MyEvent from "./components/myEvent/MyEvent.vue";
-import FindEvent from "./components/findEvent/FindEvent.vue";
+import Event from "./components/event/Event.vue";
 import Categories from "./components/categories/Categories.vue";
 import SignOut from "./components/signOut/SignOut.vue";
 
@@ -12,7 +12,7 @@ import SignOut from "./components/signOut/SignOut.vue";
 const app = createApp(App);
 app.component("navigation", Navigation);
 app.component("my-event", MyEvent);
-app.component("find-event", FindEvent);
+app.component("event", Event);
 app.component("categories", Categories);
 app.component("sign-Out", SignOut);
 
@@ -21,7 +21,7 @@ const router = createRouter({
     routes: [
         { path: '/', component: Navigation},
         { path: '/myevent', component: MyEvent},
-        { path: '/findevent', component: FindEvent},
+        { path: '/event', component: Event},
         { path: '/categories', component: Categories },
         { path: '/signout', component: SignOut },
     ],
