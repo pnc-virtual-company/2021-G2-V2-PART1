c4ffbacba35f49b9edb61c5de9e1e50f08bb4c38
channarypha <channary.pha@student.passerellesnumeriques.org>
2021-12-01 11:08:50 +0700

    #21 create category card and view

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+    <navigation></navigation>
+    <router-view></router-view>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import Navigation from './components/menu/Navigation.vue';
+  export default {
+  components: { Navigation },
+  // components: { Navigation },
+  data() {
+    return {
+      categories: [
+        { id: 1, name: "Manuel Lorenz", age: 21 },
+        { id: 2, name: "Julie Jone", age: 31 },
+      ],
+    };
+  },
+  provide() {
+    return { categories: this.categories };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background: silver;
+  }
 </style>
