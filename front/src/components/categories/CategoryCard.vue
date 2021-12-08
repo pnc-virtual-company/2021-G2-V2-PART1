388fc25a44b7df25eddcf6726653532407c05e48
vichekalo <vicheka.lo@student.passerellesnumeriques.org>
2021-12-08 18:07:16 +0700

    #00 edit and delete confirmation dialog

--- front/src/components/categories/CategoryCard.vue
+++ front/src/components/categories/CategoryCard.vue
@@ -5,19 +5,76 @@
         <h2>{{ ctr.name }}</h2>
       </div>
       <div class="change" >
-        <i id="edit-icon" class="fa fa-pencil" @click="showFormCategory(ctr)"></i>
-        <i id="delete-icon" @click="$emit('deleteCategory', ctr.id)" class="fa fa-trash"></i>
+        <i id="edit-icon" class="fa fa-pencil" @click="showEditRessource(ctr.name, ctr.id)"></i>
+        <i id="delete-icon" class="fa fa-trash" @click="showDeleteRessource(ctr.id)"></i>
       </div>
     </div>
+
+    <base-dialog v-if="dialogDisplayed" :title="dialogTitle" @close="closeDialog">
+      <label for="description" v-if="dialogMode === 'edit'">Category Name</label>
+      <textarea v-model="dialogTextField" id="description" name="description" rows="3" ref="descInput" v-if="dialogMode === 'edit'"></textarea>
+      <h2 id="ask" v-if="dialogMode !== 'edit' "><i class="fa fa-exclamation-triangle" ></i> Do you want to delete this category?</h2>
+      <template #actions>
+        <button @click="onConfirm">{{ dialogButtton }}</button>
+      </template>
+    </base-dialog>
   </section>
 </template>
+
 <script>
+
+import BaseDialog from '../UI/BasDialog.vue';
+
 export default {
   props: ['category'],
-  emits:['deleteCategory', 'editCategory', "showFormCate", "showForm"],
+  emits:['editCategory', "showFormCate", "showForm", "delete"],
+  components:{BaseDialog},
+  data(){
+    return{
+      dialogMode: 'edit', 
+      dialogDisplayed: false,
+      dialogTextField: '',
+      categoryId: null,
+    }
+  },
+  computed: {
+    dialogTitle() {
+      return this.dialogMode === 'edit' ? 'EDIT RESSOURCE' : 'DELETE RESSOURCE';
+    },
+    dialogButtton() {
+      return this.dialogMode === 'edit' ? 'EDIT' : 'DELETE';
+    },
+  },
   methods: {
-    showFormCategory(ctr) {
-      this.$emit("showFormCate",ctr);
+   
+    closeDialog() {
+      this.dialogDisplayed = false;
+      this.categoryId = null;
+    },
+    showEditRessource(categoryName, id) {
+      this.dialogMode = 'edit';
+      this.dialogTextField = categoryName;
+      this.dialogDisplayed = true;
+      this.categoryId = id;
+
+    },
+
+    showDeleteRessource(id) {
+      this.dialogMode = 'delete';
+      this.dialogDisplayed = true;
+      this.categoryId = id;
+    },
+
+    onConfirm() {
+      if (this.dialogMode === 'edit') {
+        console.log(' edited');
+        this.$emit('editCategory', this.dialogTextField, this.categoryId);
+      } else {
+        console.log(this.categoryId);
+        this.$emit('delete', this.categoryId);
+      }
+      this.closeDialog();
+      
     },
   },
 };
@@ -72,4 +129,55 @@ export default {
     text-align: center;
     display: none;
   }  
+
+
+  /*  */
+  .form-control {
+    margin: 0.5rem 0;
+    }
+
+    label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    }
+
+    input,
+    textarea {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    }
+
+    input:focus,
+    textarea:focus {
+    background-color: #f0e6fd;
+    outline: none;
+    border-color: #3d008d;
+    }
+
+    h3 {
+    margin: 0.5rem 0;
+    font-size: 1rem;
+    }
+
+    button{
+      padding: 15px;
+      border: none;
+      font-weight: bold;
+      background: orange;
+      color: white;
+      outline: none;
+      width: 20%;
+      border-radius: 10px;
+    }
+    .fa-exclamation-triangle{
+      color: red;
+      margin-right: 10px;
+      font-size: 30px;
+    }
+    #ask{
+      text-align: center;
+    }
 </style>
