292149e030c29b540cd706dd458c15ddcda74b4b
Lyden Chai <lyden.chai@PNCLB12-23.pnc.lan>
2021-12-13 00:12:25 +0700

    #00 fixed errors after merge from other branch

--- front/src/components/UI/BaseButton.vue
+++ front/src/components/UI/BaseButton.vue
@@ -5,35 +5,36 @@
 </template>
 
 <script>
-export default {
-  props: ['mode'],
-};
+  export default {
+    props: ['mode'],
+  };
 </script>
 
 <style scoped>
-button {
-  padding: 0.75rem 1.5rem;
-  font-family: inherit;
-  background-color: var(--main-color);
-  border: 1px solid var(--main-color);
-  color: white;
-  cursor: pointer;
-}
 
-button:hover,
-button:active {
-  background-color: var(--main-color-light);
-  border-color: var(--main-color-light);
-}
+  button {
+    padding: 0.75rem 1.5rem;
+    font-family: inherit;
+    background-color: var(--main-color);
+    border: 1px solid var(--main-color);
+    color: white;
+    cursor: pointer;
+  }
 
-.flat {
-  background-color: transparent;
-  color: var(--main-color);
-  border: none;
-}
+  button:hover,
+  button:active {
+    background-color: var(--main-color-light);
+    border-color: var(--main-color-light);
+  }
 
-.flat:hover,
-.flat:active {
-  background-color: #edd2ff;
-}
+  .flat {
+    background-color: transparent;
+    color: var(--main-color);
+    border: none;
+  }
+
+  .flat:hover,
+  .flat:active {
+    background-color: #edd2ff;
+  }
 </style>
